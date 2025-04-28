0b50dbb40b94d1b6b76a53ff41ccc3dc64adf1a3
Unknown Developer <138309645+Unknown-Dev0001@users.noreply.github.com>
2025-04-28 19:08:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 from os import environ
-from typing import List
+from dotenv import load_dotenv
 
-API_ID = int(environ.get("API_ID", ""))
+load_dotenv()
+
+API_ID = int(environ.get("API_ID", 0))
 API_HASH = environ.get("API_HASH", "")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER_ID = int(environ.get("OWNER_ID", ""))
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+OWNER_ID = int(environ.get("OWNER_ID", 0))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", 0))
 MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
 PORT = int(environ.get('PORT', 8080))
-IS_FSUB = bool(environ.get("FSUB", True)) # Set "True" For Enable Force Subscribe
-AUTH_CHANNELS = list(map(int, environ.get("AUTH_CHANNEL", "-100xxxxxxxxx -100xxxxxxx").split())) # Add Multiple channel id
+
+IS_FSUB = environ.get("FSUB", "True").lower() == "true"  # Correct way to parse boolean
+AUTH_CHANNELS = list(map(int, environ.get("AUTH_CHANNELS", "-1001234567890").split()))
